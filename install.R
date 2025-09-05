dfdb186cbd65e3c0d9e030a94c6a5030f6e0058b
Jeeya Shah <111391121+jrshah1@users.noreply.github.com>
2025-09-05 10:54:30 -0400

    Update install.R

--- install.R
+++ install.R
@@ -2,5 +2,6 @@ install.packages("tidyverse", dependencies = TRUE)
 install.packages("rmarkdown", dependencies = TRUE)
 install.packages("IRkernel", dependencies = TRUE)
 install.packages("shiny"), dependencies = TRUE)
+install.packages("readr")
 
 IRkernel::installspec(user = FALSE)
