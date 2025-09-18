977ddb4508004ad4b4dfc7fe3132f5476cf29b93
Jeeya Shah <111391121+jrshah1@users.noreply.github.com>
2025-09-18 13:20:17 -0400

    Update install.R

--- install.R
+++ install.R
@@ -4,5 +4,7 @@ install.packages("IRkernel", dependencies = TRUE)
 install.packages("shiny", dependencies = TRUE)
 install.packages("readr", dependencies = TRUE)
 install.packages("skimr", dependencies = TRUE)
+install.packages("Rtsne", dependencies = TRUE)
+
 
 IRkernel::installspec(user = FALSE)
