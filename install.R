10bea74fd8e1db08569c2c39454e7199436fc704
Jeeya Shah <111391121+jrshah1@users.noreply.github.com>
2025-11-15 17:10:47 -0500

    Update install.R

--- install.R
+++ install.R
@@ -6,6 +6,14 @@ install.packages("readr", dependencies = TRUE)
 install.packages("skimr", dependencies = TRUE)
 install.packages("Rtsne", dependencies = TRUE)
 install.packages("glmnet", dependencies = TRUE)
+install.packages("httr", dependencies = TRUE)
+install.packages("jsonlite", dependencies = TRUE)
+install.packages("tokenizers", dependencies = TRUE)
+install.packages("stringr", dependencies = TRUE)
+install.packages("R6", dependencies = TRUE)
+install.packages("viridis", dependencies = TRUE)
+
+
 
 
 IRkernel::installspec(user = FALSE)
